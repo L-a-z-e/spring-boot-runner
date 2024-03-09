ff0d4afd03519ecb349c13d4a8d988c286198dad
L-a-z-e <yysi8771@gmail.com>
2024-03-09 17:04:19 +0900

    first commit

---
+++ src/test/java/com/laze/springbootrunner/SpringBootRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laze.springbootrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
